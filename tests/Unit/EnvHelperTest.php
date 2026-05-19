92b4941f7bec9df2e070ede35da34468f78e6166
Princex25 <ouldmoulayemoulayeahmed@gmail.com>
2026-05-19 18:48:58 +0000

    test: trigger webhook

--- tests/Unit/EnvHelperTest.php
+++ tests/Unit/EnvHelperTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 
-require_once __DIR__ . '/../../shared/config.php';
+require_once __DIR__ . '/../../devops-project/shared/config.php';
 
 final class EnvHelperTest extends TestCase
 {
